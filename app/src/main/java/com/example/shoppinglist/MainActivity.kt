b9f7efbc069578035abee76c9f47b7bdb7aa473c
David Port Louis <dportlouis@gmail.com>
2020-05-30 16:31:56 +0530

    30days-of-kotlin --final commit

---
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.shoppinglist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.SparseBooleanArray
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var itemList = arrayListOf<String>()
+        var adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_multiple_choice,itemList)
+
+        add.setOnClickListener{
+            itemList.add(editText.text.toString())
+            listView.adapter = adapter
+            adapter.notifyDataSetChanged()
+            editText.text.clear()
+        }
+
+        clear.setOnClickListener{
+            itemList.clear()
+            adapter.notifyDataSetChanged()
+        }
+
+        listView.setOnItemClickListener{ _, _, i, _ ->android.widget.Toast.makeText(this,"You Selected" +
+                " the item: "+ itemList[i],android.widget.Toast.LENGTH_SHORT).show()}
+
+        delete.setOnClickListener{
+            val position: SparseBooleanArray = listView.checkedItemPositions
+            val count = listView.count
+            var item  = count - 1
+            while (item >= 0){
+                if(position[item])
+                {
+                    adapter.remove(itemList[item])
+                }
+                item--
+            }
+            position.clear()
+            adapter.notifyDataSetChanged()
+        }
+    }
+}
